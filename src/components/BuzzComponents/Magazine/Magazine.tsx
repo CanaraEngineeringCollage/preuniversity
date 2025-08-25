373ab4fe0ac9589390eafe387b7dda8112994773
sidharthmk24 <sidharthmk24@gmail.com>
2025-08-25 18:20:14 +0530

    student magazine and exam curiculars added

---
+++ src/components/BuzzComponents/Magazine/Magazine.tsx
@@ -0,0 +1,10 @@
+
+ 
+
+ export default function Magazine() {
+    return (
+        <div className="w-full  h-screen">
+        <iframe className="w-full h-full" allowfullscreen ="allowfullscreen" allow="clipboard-write" scrolling="no" src="https://heyzine.com/flip-book/b0ea8c8c69.html" frameborder="0"></iframe>
+        </div>
+    )
+}
